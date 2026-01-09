7f11d1864f1b30c7dae4d03944d7f457d8c0b99f
A25LiaLe <a25liale@student.his.se>
2026-01-09 10:02:25 +0100

    Scenbyte (+2), En egen scen med kort bakgrundsstory och startmeny scen

---
+++ Assets/Scripts/ApplicationHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This component can be used to access methods for changing the scene, and quitting the game
+// It can be used together with UnityEventOnTrigger, or UI-button-events, to decide when a scene should be changed or the game should be closed
+public class ApplicationHandler : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        // Load the scene named "NewScene"
+        SceneManager.LoadSceneAsync("StoryScene", LoadSceneMode.Single);
+    }
+    
+    public void ChangeScene2()
+    {
+        // Load the scene named "NewScene"
+        SceneManager.LoadSceneAsync("a25liale", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
